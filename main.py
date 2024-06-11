826b95e6c4c821a7a56a60d27d9f4db90396b0df
Florin - Sebastian Serban <serbanf@bzz.ch>
2024-06-11 13:42:12 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import tkinter as tk
-
+from datetime import date
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Notenrechner')
-        self.geometry('1000x1000')
+        self.geometry('963x1000')
+        self.resizable(False, False)
 
         self.label = tk.Label(self, text='Willommen zum Notenrechner', font=25)
         self.frame = tk.Frame(self, bg="yellow")
         self.label.pack()
-        self.frame.pack(fill="both", expand=True)
+        self.frame.pack(fill="x")
 
 
         #Buttons
@@ -46,11 +47,11 @@ class Window(tk.Tk):
 
 class Grade(tk.Frame):
     def __init__(self, parent, window):
-        super().__init__(master=parent, relief="raised")
+        super().__init__(master=parent, relief="raised", bg="blue")
         self.weight = 0.0
         self.grade = 0.0
         self.name = None
-        self.date = None
+        self.date = date.today().strftime('%d.%m.%Y')
         self.window = window
         self.window.add()
 
@@ -60,10 +61,49 @@ class Grade(tk.Frame):
         self.grade_container = tk.Frame(self, relief="raised", padx=10, bd=2)
         self.grade_container.grid(column=1, row=0)
 
-        self.label = tk.Label(self.grade_container, text="Grade", font='Helvetica 15')
-        self.grade_field = tk.Entry(self.grade_container, name="note", font='Helvetica 15', width=7)
-        self.label.grid(column=0, row=0, padx=(0, 12))
+        self.grade_label = tk.Label(self.grade_container, text="Grade", font='Helvetica 15')
+        self.gradevar = tk.StringVar(value="")
+        self.gradevar.trace("w", self.update)
+        self.grade_field = tk.Entry(self.grade_container, name="note", font='Helvetica 15', width=7, textvariable=self.gradevar)
+        self.grade_label.grid(column=0, row=0, padx=(0, 12))
         self.grade_field.grid(column=1, row=0, padx=(0, 7))
+
+        self.weight_container = tk.Frame(self, relief="raised", padx=10, bd=2)
+        self.weight_container.grid(column=2, row=0)
+
+        self.weight_label = tk.Label(self.weight_container, text="Weight", font='Helvetica 15')
+        self.weightvar = tk.StringVar(value="1.0")
+        self.weightvar.trace("w", self.update)
+        self.weight_field = tk.Entry(self.weight_container, name="weight", font='Helvetica 15', width=7, textvariable=self.weightvar)
+        self.weight_label.grid(column=0, row=0, padx=(0, 12))
+        self.weight_field.grid(column=1, row=0, padx=(0, 7))
+
+        self.date_container = tk.Frame(self, relief="raised", padx=10, bd=2)
+        self.date_container.grid(column=3, row=0)
+
+        self.date_label = tk.Label(self.date_container, text="Date", font='Helvetica 15')
+        self.datevar = tk.StringVar(value=self.date)
+        self.datevar.trace("w", self.update)
+        self.date_field = tk.Entry(self.date_container, name="date", font='Helvetica 15', width=10, textvariable=self.datevar)
+        self.date_label.grid(column=0, row=0, padx=(0, 12))
+        self.date_field.grid(column=1, row=0, padx=(0, 7))
+
+        self.name_container = tk.Frame(self, relief="raised", padx=10, bd=2)
+        self.name_container.grid(column=4, row=0, sticky="e")
+
+        self.name_label = tk.Label(self.name_container, text="Title", font='Helvetica 15')
+        self.namevar = tk.StringVar(value="")
+        self.namevar.trace("w", self.update)
+        self.name_field = tk.Entry(self.name_container, name="name", font='Helvetica 15', textvariable=self.namevar)
+        self.name_label.grid(column=0, row=0, padx=(0, 12))
+        self.name_field.grid(column=1, row=0, padx=(0, 7), sticky="e")
+
+    def update(self, *args):
+        self.weight = self.weight_field.get()
+        self.grade = self.grade_field.get()
+        self.name = self.weight_field.get()
+        self.date = self.date_field.get()
+
 
 
 
